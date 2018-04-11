1327d57a5cd24e06a34a685cfd59769dff8d10f2
DESKTOP-PO2B61U\cooli <arg17801@uvg.edu.gt>
2018-04-11 08:54:44 -0600

    Laboratorio 2 Extra: Texto con puntaje

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -1,21 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BirdScript : MonoBehaviour {
 
     public float jumpForce = 200f;
     private Rigidbody2D rb;
+    public Text scoreText;
 
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
 	}
 	
 	void Update () {
-        if (Input.GetButtonDown("Jump")) {
-            rb.velocity = Vector2.zero;
-            rb.AddForce(Vector2.up * jumpForce);
+        if (GameController.instance.gameOver == false) {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(Vector2.up * jumpForce);
+            }
         }
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameController.instance.score++;
+        Debug.Log("Score: " + GameController.instance.score);
+        scoreText.text = "Score: " + GameController.instance.score;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameController.instance.gameOver = true;
+    }
 
 }
